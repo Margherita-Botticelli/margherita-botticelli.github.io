083c186df9a6872362f6e44f7afd381b1323a64a
Margherita-Botticelli <mxb1267@student.bham.ac.uk>
2026-06-24 11:34:45 +0100

    Collapse animation abstracts

--- JS.js
+++ JS.js
@@ -1,6 +1,14 @@
-const toggle = document.querySelector('.toggle');
-const content = document.querySelector('.abstract');
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-toggle.addEventListener('click', (e) => {
-    content.toggleAttribute('open');
-});
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var  abstract = this.nextElementSibling;
+    if ( abstract.style.maxHeight){
+       abstract.style.maxHeight = null;
+    } else {
+       abstract.style.maxHeight =  abstract.scrollHeight + "px";
+    }
+  });
+}
